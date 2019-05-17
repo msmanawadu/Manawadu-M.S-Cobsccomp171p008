7c2066465137c1aa83b746c3fe2a8bdd6bb5df2b
Sandeepa Manawadu <msmanawadu@gmail.com>
2019-05-17 17:25:18 +0530

    Project Initialization with Firebase
    Initialized the project with Firebase and Pods

---
+++ Manawadu M.S-Cobsccomp171p008/Manawadu M.S-Cobsccomp171p008/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Manawadu M.S-Cobsccomp171p008
+//
+//  Created by Sandeepa Manawadu on 2019/05/17.
+//  Copyright © 2019 Sandeepa Manawadu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Manawadu M.S-Cobsccomp171p008/Pods/Target Support Files/Pods-Manawadu M.S-Cobsccomp171p008/Pods-Manawadu M.S-Cobsccomp171p008-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Manawadu_M_S_Cobsccomp171p008 : NSObject
+@end
+@implementation PodsDummy_Pods_Manawadu_M_S_Cobsccomp171p008
+@end
