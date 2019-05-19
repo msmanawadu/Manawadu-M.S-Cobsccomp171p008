b6d357d0dedd1b429d199fb8f049c4885b8c4416
Sandeepa Manawadu <msmanawadu@gmail.com>
2019-05-19 18:11:25 +0530

    Student profile picture uploading done

--- Manawadu M.S-Cobsccomp171p008/Manawadu M.S-Cobsccomp171p008/controllers/addStudentDataViewController.swift
+++ Manawadu M.S-Cobsccomp171p008/Manawadu M.S-Cobsccomp171p008/controllers/addStudentDataViewController.swift
@@ -17,6 +17,7 @@ class addStudentDataViewController: UIViewController {
     var imgPick: UIImagePickerController!
     var imageDownloadURL: String = ""
     var dataBaseRef: DatabaseReference!
+    var rootImage: String = ""
 
     
     // Student Data variables
@@ -26,30 +27,23 @@ class addStudentDataViewController: UIViewController {
     var phoneNo: Int = 0
     var cityOf: String = ""
     var profileImgURL: String = ""
+ 
     
-    
+    // property observer method to validate the image download
+    var didSetVariable = 0 {
+        didSet{
+            self.saveStudentData()
+        }
+    }
     
     @IBOutlet weak var profileImage: UIImageView!
-    
     @IBOutlet weak var doBtnAddPhoto: UIButton!
-    
     @IBOutlet weak var fName: UITextField!
-    
     @IBOutlet weak var lName: UITextField!
-    
-    
     @IBOutlet weak var phoneNumber: UITextField!
-    
-    
     @IBOutlet weak var city: UITextField!
-    
-    
     @IBOutlet weak var FbProfileURL: UITextField!
-    
-    
     @IBOutlet weak var saveBtn: UIButton!
-    
-    
     @IBOutlet weak var cancelBtn: UIButton!
     
     
@@ -57,19 +51,24 @@ class addStudentDataViewController: UIViewController {
     @IBAction func doBtnAddPhoto(_ sender: UIButton) {
     }
     
-    
     @IBAction func doBtnSave(_ sender: UIButton) {
+        
+        self.firstName = self.fName.text!
+        self.lastName = self.lName.text!
+        self.fbProfileURL = self.FbProfileURL.text!
+        self.phoneNo = Int(self.phoneNumber.text!)!
+        self.cityOf = self.city.text!
+        
+        imageUploading()
+        
+        // Testing the passed values in the console
+        print(firstName, lastName, fbProfileURL, phoneNo, cityOf)
+        
     }
-    
     
     @IBAction func doBtnCancel(_ sender: UIButton) {
     }
     
-    
-    
-    
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -109,18 +108,90 @@ class addStudentDataViewController: UIViewController {
     @objc func openImgPick(_sender: Any) {
         self.present(imgPick, animated: true, completion: nil)
     }
+
+    // Image uploading function
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func imageUploading() {
+        
+        let storageReference = Storage.storage().reference()
+        
+        // Data in memory
+        
+        guard let data = self.profileImage.image!.jpegData(compressionQuality: 0.75) else {return}
+        let image: UIImage = self.profileImage.image!
+        print("Image Data: \(image)")
+        
+        
+        // create a reference to the file you want to upload
+        let selectedImage = storageReference.child("profile images/\(rootImage)")
+        
+        // upload the file the path "profile images/selectedImage.jpg"
+        
+        let uploadTask = selectedImage.putData(data, metadata: nil) {
+            (metadata, error) in
+            guard let metadata = metadata else {
+                // when error ocuured
+                return
+            }
+            
+    print("Meta Data:\(metadata)")
+            
+    // metadata contains file meta data such as size, content-type
+            
+            let size = metadata.size
+            
+    // You can also access to download URL after upload
+       
+            let url = selectedImage.downloadURL{(url, error) in
+                
+                guard let downloadURL = url else {
+                    
+                    print("Error Img: \(error)")
+                    return
+                }
+                
+       self.imageDownloadURL = downloadURL.absoluteString
+                
+                if self.imageDownloadURL != "" {
+                    self.didSetVariable = 1
+                    
+                }
+                print("Download URL \(self.imageDownloadURL)")
+            }
+            print("URL: \(self.imageDownloadURL)")
+        }
     }
-    */
-
+    
+    
+    // Firebase RTDB data saving function
+    func saveStudentData() {
+        
+    dataBaseRef = Database.database().reference().child("StudentData/Students").childByAutoId()
+        
+        // passing data as a JSON key-value pair
+        
+        let student = ["fName": self.firstName,
+                       "lName": self.lastName,
+                       "fbProfileURL": self.fbProfileURL,
+                       "phoneNumber": self.phoneNo,
+                       "city":  self.cityOf,
+        "profileImageURL": self.imageDownloadURL] as [String: Any]
+        
+        print(student)
+        
+        self.dataBaseRef!.setValue(student) {error, ref in
+            
+            if error != nil{
+                let alert = UIAlertController(title: "Login Error", message: error?.localizedDescription, preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+                
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
 }
 
 extension addStudentDataViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
@@ -133,16 +204,14 @@ extension addStudentDataViewController: UIImagePickerControllerDelegate, UINavig
             print("Image Name: \(imageName)")
             let imageExtension = imageURL.pathExtension
             print("Image Extension: \(imageExtension)")
+            self.rootImage = imageName
         }
         
         if let pickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage{
             self.profileImage.image = pickedImage
         }
-        
         picker.dismiss(animated: true, completion: nil)
-        
     }
-    
 }
 
 
